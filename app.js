7c4c5d59d15389d0439b914b25842cd21f889daf
Xin Qi <qi.xin1@northeastern.edu>
2023-12-03 19:44:12 -0800

    fix cookie

--- app.js
+++ app.js
@@ -16,14 +16,21 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
   }
  ));
- const sessionOptions = {
+const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
-  };
-  app.use(
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+}  
+app.use(
     session(sessionOptions)
-  );
+);
   
 app.use(express.json());
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'

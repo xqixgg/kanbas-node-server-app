3488417a9f5fdc9be6cc6a6dfeda3d7c2e27a1df
Xin Qi <qi.xin1@northeastern.edu>
2023-12-03 18:40:28 -0800

    configure frontend url

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import session from "express-session";
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: "https://astounding-hummingbird-d3f973.netlify.app",
+    origin: process.env.FRONTEND_URL,
   }
  ));
  const sessionOptions = {

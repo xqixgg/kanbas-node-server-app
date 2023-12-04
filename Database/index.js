ca32189f5f72cbcfdc749e9c1c20db50a4eac5a3
Xin Qi <qi.xin1@northeastern.edu>
2023-12-03 17:00:13 -0800

    a6

--- Database/index.js
+++ Database/index.js
@@ -1,3 +1,4 @@
+
 import courses from "./courses.json" assert { type: "json" };
 import modules from "./modules.json" assert { type: "json" };
 import assignments from "./assignments.json" assert { type: "json" };
--- app.js
+++ app.js
@@ -2,13 +2,35 @@ import express from 'express';
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import mongoose from 'mongoose';
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
+import UserRoutes from "./users/routes.js";
 import "dotenv/config";
+import session from "express-session";
+
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  }
+ ));
+ const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+  
 app.use(express.json());
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+UserRoutes(app);
+
 Hello(app)
 CourseRoutes(app);
 ModuleRoutes(app);
